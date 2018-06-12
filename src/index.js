115f46290ec8ac01edf25f82528b12aae3b93470
zixiong liu <lzx@uchicago.edu>
2018-06-12 17:33:02 -0500

    user

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import express from "express";
-import bodyParser from "body-parser";
+"use strict";
+const config = require("./config.js");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const MongoClient = require("mongodb");
+
+const redis = require("redis");
+const redisClient = redis.createClient(config.redis.url);
+
+const signup_router = require("./signup.js");
+const login_router = require("./login.js");
+
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
+app.use(cookieParser());
 
-app.get('/api', (req, res) => res.json({}));
+app.get("/api", (req, res) => res.json({}));
 
-app.post('/api/signup', (req, res) => {
-    console.log(req.body);
-    res.json({status: "OK"});
+app.use("/api/signup", signup_router);
+
+app.use("/api/login", login_router);
+
+redisClient.on("error", err => {
+    console.log("Redis Error: " + err);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.locals.cache = redisClient;
+
+MongoClient.connect(config.database.url)
+    .catch(err => console.error(err.stack))
+    .then(client => {
+        app.locals.db = client.db("test");
+        app.listen(3000, () => console.log("Listening on port 3000..."));
+    });
+
